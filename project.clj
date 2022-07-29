886f9290efb37790975e2187c19f8f4d6a57aa8a
Andrey Krainyak <mode.andrew@gmail.com>
2022-07-29 19:29:06 +0800

    Leiningen project file
    Define the package and its dependencies.

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject lwjgl-clj "0.1.0"
+  :dependencies [
+    [org.clojure/clojure "1.11.1"]
+    [org.lwjgl/lwjgl "3.3.1"]
+    [org.lwjgl/lwjgl "3.3.1" :classifier "natives-linux"]
+    [org.lwjgl/lwjgl-glfw "3.3.1"]
+    [org.lwjgl/lwjgl-glfw "3.3.1" :classifier "natives-linux"]
+    [org.lwjgl/lwjgl-opengl "3.3.1"]
+    [org.lwjgl/lwjgl-opengl "3.3.1" :classifier "natives-linux"]
+    [org.lwjgl/lwjgl-vulkan "3.3.1"]
+  ])
